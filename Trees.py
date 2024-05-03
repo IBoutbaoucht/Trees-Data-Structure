2700572ca82b52b79bdfce62902386b321d67d24
IBoutbaoucht <167520204+IBoutbaoucht@users.noreply.github.com>
2024-05-03 09:03:45 +0100

    Update Trees.py

--- Trees.py
+++ Trees.py
@@ -1,1 +1,9 @@
-print("Salam Tarik Labas")
+print("")
+Tree = [7,[5,[4,None,None],[6,None,None]],[9,[8,None,None],[10,None,None]]]
+A = Tree
+while A : 
+  col = A.pop()
+  print(col[0])
+  A.append(col[1])
+  A.append(col[2])
+  
